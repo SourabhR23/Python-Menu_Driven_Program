1063930779e2c523ae9d16e4a11a9c8bcfa66d0e
SourabhR23 <64792134+SourabhR23@users.noreply.github.com>
2020-08-23 00:04:17 +0530

    updated

--- menu_driven_program.py
+++ menu_driven_program.py
@@ -1,70 +1,60 @@
 # importing required libraries
 # 'pyttsx3' is a text-to-speech conversion library
 import pyttsx3
-# 'os' is system in-build library
+# 'os' is python in-build library
 import os
 
 voice = pyttsx3.init()
 Voices = voice.getProperty('voices')
 # Setting up the voice speed rate
-voice.setProperty('rate', 125)   
+voice.setProperty('rate', 170)   
 # Setting up the volume of the voice
 voice.setProperty('volume', 0.8)
-# Female voice
-# If you want to change voice to female, please remove the '#' from below command
+# For having Female voice
 #voice.setProperty('voice', Voices[1].id)
 
 # Introduction
-x = "Welcome to the menu driven program!\
-    From given list below, please select the application which you want to open."
+x = "Welcome to the menu driven program!\nI am here to direct you to the application you want to open."
 voice.say(x)
+print(x)
 voice.runAndWait()
-
-# List of applications
-a = ['Google Chrome', 'Paint', 'Windows Media Player', 'Notepad']
-n = 0
-for apps in a:
-    voice.say(a[n])
-    print(a[n])
-    n += 1
-    voice.runAndWait()
+pyttsx3.speak("Please enter the application: ")
     
 while True:
     # Taking input from the user
-    p = input("\nPlease enter the input: ")
+    p = input("\nPlease enter the application you want to open: ")
+    
     # For 'Chrome' application
     if ('run' in p.lower() or 'open' in p.lower() or 'execute' in p.lower()) and ("chrome" in p.lower()):
-        pyttsx3.speak("Opening Chrome.")
+        pyttsx3.speak("OK. Opening Chrome.")
         os.system("Chrome")
         
     # For 'Paint' application
     elif ('run' in p.lower() or 'open' in p.lower() or 'execute' in p.lower()) and ("paint" in p.lower()):
-        pyttsx3.speak("Opening Paint.")
+        pyttsx3.speak("OK. Opening Paint.")
         os.system("mspaint")
         
     # For 'Windows media player' application
     elif ('run' in p.lower() or 'open' in p.lower() or 'execute' in p.lower()) and ("windows media player" in p.lower() 
            or "wmplayer" in p.lower() or "media player" in p.lower()):
-        pyttsx3.speak("Opening windows media player.")
+        pyttsx3.speak("OK. Opening windows media player.")
         os.system("Wmplayer")
         
     # For 'Notepad' application
     elif ('run' in p.lower() or 'open' in p.lower() or 'execute' in p.lower()) and ("notepad" in p.lower()):
-        pyttsx3.speak("Opening Notepad.")
+        pyttsx3.speak("OK. Opening Notepad.")
         os.system("notepad")
+    
+    # For 'Calculator' application
+    elif ('run' in p.lower() or 'open' in p.lower() or 'execute' in p.lower()) and ("calculator" in p.lower()):
+        pyttsx3.speak("OK. Opening Calculator.")
+        os.system("calc")
         
-    # Exit/Quitting of application 
+    # Exit/Quitting of application codition
     elif ('exit' in p.lower() or 'close' in p.lower() or 'quit' in p.lower()):
-        pyttsx3.speak("Thank you for using the application!")
+        pyttsx3.speak("OK! Thank you for using the application! See you soon!")
         break
         
     else:
         pyttsx3.speak("Please enter the valid input!")
         print("Please enter the valid input!")
-
-
-# Sample inputs
-# please open Chrome
-# I want to draw something so run PAINT application
-# Let's dance, execute media player
-# Close the program
